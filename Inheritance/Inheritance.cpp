630a6587db8f5c72cbcd585b5711676c84bad42c
hikmatyar22 <abiq5161@gmail.com>
2024-05-21 18:19:05 +0700

    membuat class pelajar dengan mewarisi dari class orang dan membernya bersofat piublic

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -16,9 +16,41 @@ public:
 		return a + b;
 	}
 };
+#include <iostream>
+#include <string>
+using namespace std;
 
+class orang {
+public:
+	string nama;
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+	}
+};
 
 int main()
 {
-	
+
 }
