8a3c5be47127f2089e080089e61b73d36ef6d559
hikmatyar22 <abiq5161@gmail.com>
2024-05-21 11:41:07 +0700

    membuat int main dan memanggil semua program untuk menjalankannya

--- Aggregation/Aggregation.cpp
+++ Aggregation/Aggregation.cpp
@@ -6,6 +6,13 @@ using namespace std;
 #include "ibu.h"
 
 int main() {
-	
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
 
 }
