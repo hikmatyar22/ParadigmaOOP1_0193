b2f43a2e830ad954b39a905edabddfbef11e76ce
hikmatyar22 <abiq5161@gmail.com>
2024-05-21 18:30:03 +0700

    menghapus program yang double

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -16,24 +16,6 @@ public:
 		return a + b;
 	}
 };
-#include <iostream>
-#include <string>
-using namespace std;
-
-class orang {
-public:
-	string nama;
-	orang(string pNama) :
-		nama(pNama) {
-		cout << "orang dibuat\n" << endl;
-	}
-	~orang() {
-		cout << "orang dihapus\n" << endl;
-	}
-	int jumlah(int a, int b) {
-		return a + b;
-	}
-};
 
 class pelajar : public orang {
 public:
